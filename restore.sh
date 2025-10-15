f9651812ccabaf57d12f70c8ac525687ce7b88b5
Collin Hawks <76015895+collinhawks@users.noreply.github.com>
2025-10-14 22:33:03 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+# Restore from cloud storage
+if rclone --config ~/.config/rclone/rclone.conf ls remote:navi-backup/data > /dev/null 2>&1; then
+    echo "Downloading backup from cloud..."
+    rclone --config ~/.config/rclone/rclone.conf sync remote:navi-backup/data /data --progress#!/bin/bash
 set -e
 
 echo "Starting restore process..."
